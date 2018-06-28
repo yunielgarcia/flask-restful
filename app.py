fab4b8d68fb25c890fc70ddba1a2ee147ff00ade
Yuniel Garcia <garciayuni84@gmail.com>
2018-06-28 17:42:47 -0400

    dummy commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 import models
+from resources.courses import courses_api
+from resources.reviews import reviews_api
 
 DEBUG = True
 PORT = 8800
@@ -8,7 +10,8 @@ HOST = '0.0.0.0'
 
 app = Flask(__name__)
 app.secret_key = 'da;lfkjadlfdsafasdfjfdaopsifdsaifdoisafakldf=jdaff'
-
+app.register_blueprint(courses_api)
+app.register_blueprint(reviews_api, url_prefix='/api/v1')
 
 @app.route('/')
 def hello_world():
--- resources/reviews.py
+++ resources/reviews.py
@@ -1,10 +1,36 @@
-from flask import jsonify
-from flask.ext.restful import Resource
+from flask import jsonify, Blueprint
+from flask_restful import Resource, Api, reqparse, inputs, fields
 
 import models
 
 
 class ReviewList(Resource):
+    def __init__(self):
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument(
+            'course',
+            type=inputs.positive,
+            required=True,
+            help='No course provided',
+            location=['form', 'json']
+        )
+        self.reqparser.add_argument(
+            'rating',
+            type=inputs.int_range(1, 5),
+            required=True,
+            help='No rating provided',
+            location=['form', 'json']
+        )
+        self.reqparser.add_argument(
+            'comment',
+            type=inputs.int_range(1, 5),
+            required=False,
+            nullable=True,
+            location=['form', 'json'],
+            default=''
+        )
+        super().__init__()
+
     def get(self):
         return jsonify({'reviews': [
             {
@@ -38,3 +64,17 @@ class Review(Resource):
                 'rating': 5
             }
         )
+
+
+reviews_api = Blueprint('resources.reviews', __name__)
+api = Api(reviews_api)
+api.add_resource(
+    ReviewList,
+    '/reviews',
+    endpoint='reviews'
+)
+api.add_resource(
+    Review,
+    '/review/<int:id>',
+    endpoint='review'
+)
